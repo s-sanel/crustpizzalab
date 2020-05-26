c9fc60bc2dc7380455feb93865fafa3c2b6abd4a
Sanel Selmanovic <sanel.selmanovic@bloomingdales.com>
2020-05-26 15:56:53 -0400

    Updates

---
+++ precache-manifest.e7fc9b1c214a4a785b42dca2036f9e84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d3ebcf52958f16d11beb11ba959c7a",
+    "url": "/s-sanel/crustpizzalab/index.html"
+  },
+  {
+    "revision": "641d41b8d7125de0fa70",
+    "url": "/s-sanel/crustpizzalab/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "670fbcc600380512195f",
+    "url": "/s-sanel/crustpizzalab/static/js/2.bc94c555.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/s-sanel/crustpizzalab/static/js/2.bc94c555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "641d41b8d7125de0fa70",
+    "url": "/s-sanel/crustpizzalab/static/js/main.dd4895a7.chunk.js"
+  },
+  {
+    "revision": "1802774fb3517df00661",
+    "url": "/s-sanel/crustpizzalab/static/js/runtime-main.84f59c25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/s-sanel/crustpizzalab/static/media/logo.5d5d9eef.svg"
+  }
+]);
