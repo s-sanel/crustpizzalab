083a414731b57f1ade534649f6189bbc27d744de
Sanel Selmanovic <sanel.selmanovic@bloomingdales.com>
2020-05-28 16:06:58 -0400

    Update article styling

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,19 +6,13 @@ const Article = ({reorderForMedium}) => {
     return (
         <div className="homepage-article row m-0" style={{ }}>
           
-          <div
-            className={`article-image col-md ml-0${reorderForMedium ?' order-md-12':''}`}
-            style={{
-              backgroundColor: "beige",
-            }}>
+          <div className={`article-image col-md ml-0${reorderForMedium ?' order-md-12':''}`} style={{ backgroundColor: "beige" }}>
             <h1>slika</h1>
           </div>
 
-          <div
-            className={`article-description col-md ml-0${reorderForMedium ? ' order-md-1' : ''} `}
-            style={{  }}>
+          <div className={`article-description col-md py-3 ml-0${reorderForMedium ? ' order-md-1' : ''} `}>
             <h3>VÅRA CRUST PIZZALABS</h3>
-            <div>
+            <div className="article-description-text">
                 <p>
                     Du hittar oss i Malmö, mitt i centrum vid Hansa Companiet. <br/>
                     Vårt mål är mer krispig pizza åt folket, så vi kommer öppna fler Labs framöver. <br/>
