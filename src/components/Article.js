e5802571aa990b3f558daf6ab2fc59566cbb9a6e
Sanel Selmanovic <sanel.selmanovic@bloomingdales.com>
2020-05-28 18:38:20 -0400

    Update articles styling; pass props for title and button

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import "../styles/article.css";
 
-const Article = ({reorderForMedium}) => { 
-
+const Article = ({ reorderForMedium, title, description, buttonText, buttonUrl, children}) => { 
     return (
         <div className="homepage-article row m-0" style={{ }}>
           
           <div className={`article-image col-md ml-0${reorderForMedium ?' order-md-12':''}`} style={{ backgroundColor: "beige" }}>
-            <h1>slika</h1>
+            
           </div>
 
-          <div className={`article-description col-md py-3 ml-0${reorderForMedium ? ' order-md-1' : ''} `}>
-            <h3>VÅRA CRUST PIZZALABS</h3>
+          <div className={`article-description col-md py-5 ml-0${reorderForMedium ? ' order-md-1' : ''} `}>
+            <h3>{title}</h3>
             <div className="article-description-text">
-                <p>
-                    Du hittar oss i Malmö, mitt i centrum vid Hansa Companiet. <br/>
-                    Vårt mål är mer krispig pizza åt folket, så vi kommer öppna fler Labs framöver. <br/>
-                    Kanske öppnar vi ett Lab nära dig? Följ oss på Facebook o Instagram.
-                </p>
+                {children}
             </div>
-            <a href="#" className="btn btn-danger article-action-btn">VISA KARTA</a>
+            <a href={buttonUrl} className="btn btn-danger article-action-btn">{buttonText}</a>
           </div>
 
         </div>
--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -3,12 +3,50 @@ import Article from './Article';
 
 const ArticlesList = () => {
     return (
-        <div style={{ backgroundColor: "#FAB500" }}>
-            <Article reorderForMedium/>
-            <Article />
-            <Article reorderForMedium/>
-        </div>
-    )
+      <div style={{ backgroundColor: "#FAB500" }}>
+        <Article
+          reorderForMedium
+          title="VÅRA CRUST PIZZALABS"
+          buttonText="VISA KARTA"
+          buttonUrl="#"
+        >
+          <p>
+            Du hittar oss i Malmö, mitt i centrum vid Hansa Companiet. <br />
+            Vårt mål är mer krispig pizza åt folket, så vi kommer öppna fler
+            Labs framöver. <br />
+            Kanske öppnar vi ett Lab nära dig? Följ oss på Facebook o Instagram.
+          </p>
+        </Article>
+        
+        <Article
+            title="PIZZA FÖR DINA 5 SINNEN"
+            buttonText="LÄS MER"
+            buttonUrl="#"
+        >
+          <p>
+            Det är ingen vanlig pizza du får när du beställer från vårt Pizza
+            Lab. <br />
+            Bakom pizzorna på vår meny ligger passion och kärlek till maten.{" "}
+            <br />
+            Vi är riktiga matälskare och vi utlovar en smaksensation och massor
+            av krispig crust!
+          </p>
+        </Article>
+        
+        <Article 
+            reorderForMedium
+            title="JOBBA HOS OSS"
+            buttonText="Kontakta oss"
+            buttonUrl="#">
+                <p>
+                    Är du en matälskare, med extra försmak för Pizza?<br/> 
+                    Lagar du mat med passion och brinner för god service och gott engagemang? <br/>
+                    Då är du kanske den vi söker till vårt Pizza Lab Team! <br/>
+                    Skicka in din ansökan här
+                </p>   
+        </Article>
+      </div>
+    );
 }
 
 export default ArticlesList
