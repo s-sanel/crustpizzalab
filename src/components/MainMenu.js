cac2d5dbc5c142f4d60989d722c60bf8bbd09f63
Sanel Selmanovic <sanel.selmanovic@bloomingdales.com>
2020-05-28 03:54:53 -0400

    Update menu; autoclose when select item

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -5,106 +5,79 @@ import Nav from "react-bootstrap/Nav";
 import logo from "../logo.svg";
 import { Link } from "react-router-dom";
 import "../styles/main-menu.css";
+import PizzaIcon from "mdi-react/PizzaIcon";
+import MapIcon from "mdi-react/MapIcon";
+import EmailIcon from "mdi-react/EmailIcon";
+import ChefHatIcon from "mdi-react/ChefHatIcon";
+import MapMarkerIcon from "mdi-react/MapMarkerIcon";
+import { LinkContainer } from "react-router-bootstrap";
 
 const MainMenu = () => {
     return (
-        <div className="container" style={{ minHeight: "56px" }}>
-            <Navbar
-                // bg="light"
-                fixed="top"
-                expand="lg"
-                style={{ backgroundColor: "#FAB500" }}
-            >
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-                <Navbar.Brand
-                    as={Link}
-                    to="/"
-                    className="d-block d-lg-none p-0 ml-2 mr-auto"
-                >
-                    <img
-                        src={logo}
-                        width="40"
-                        height="40"
-                        className="d-inline-block align-top"
-                        alt="React Bootstrap logo"
-                        style={{
-                            border: "1px solid #FDC72C",
-                            borderRadius: "50%",
-                            backgroundColor: "#E74345"
-                        }}
-                    />
-                </Navbar.Brand>
+        // <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        //     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        //     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        //     <Navbar.Collapse id="responsive-navbar-nav">
+        //         <Nav className="mr-auto">
+        //             <Nav.Link href="#features">Features</Nav.Link>
+        //             <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    
+        //         </Nav>
+                
+        //     </Navbar.Collapse>
+        // </Navbar>
 
-                <Button variant="danger" className="d-block d-lg-none">
-                    ORDER ONLINE
-        </Button>
 
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav
-                        className="mr-auto"
-                        style={{ width: "100%", justifyContent: "center" }}
-                    >
-                        <Nav.Link as={Link} to="/">
-                            our pizzor
-            </Nav.Link>
-                        <Nav.Link as={Link} to="/">
-                            menus
-            </Nav.Link>
-                        <Nav.Link as={Link} to="/">
-                            contact
-            </Nav.Link>
-                        <div
-                            className="d-none d-lg-block mr-0"
-                            style={{ width: "0px" }}
-                        ></div>
-                        <Nav.Link as={Link} to="/about">
-                            work with us
-            </Nav.Link>
-                        <Nav.Link as={Link} to="/about">
-                            restaurants
-            </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+    //   <div className="container" style={{ minHeight: "56px" }}>
+        <Navbar collapseOnSelect fixed="top" expand="lg" className='main-navigation' style={{ backgroundColor: "#FAB500" }}>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Brand as={Link} to="/" className="d-block d-lg-none p-0 ml-2 mr-auto">
+            <img src={logo} alt="Crust Pizza Lab logo"
+              className="d-inline-block align-top navbar-brand-small-logo" />
+          </Navbar.Brand>
 
-                <Navbar.Brand
-                    as={Link}
-                    to="/"
-                    className="d-none d-lg-block mr-0"
-                    style={{
-                        position: "absolute",
-                        top: 0,
-                        left: "50%",
-                        transform: "translateX(-50px)",
-                        backgroundColor: ""
-                    }}
-                >
-                    <img
-                        src={logo}
-                        width="100"
-                        height="100"
-                        className="d-inline-block align-top"
-                        alt="React Bootstrap logo"
-                        style={{
-                            // position: "absolute",
-                            // top: 0,
-                            // left: 0,
-                            border: "1px solid #FDC72C",
-                            borderRadius: "50%",
-                            backgroundColor: "#E74345"
-                        }}
-                    />
-                </Navbar.Brand>
-                <Button
-                    variant="danger"
-                    className="d-none d-lg-block"
-                    style={{ position: "absolute", right: "10px" }}
-                >
-                    ORDER ONLINE
-        </Button>
-            </Navbar>
-        </div>
-    )
+          <Button variant="danger" className="d-block d-lg-none">ORDER ONLINE</Button>
+
+          <Navbar.Collapse id="basic-navbar-nav" className='navbar-collapse'>
+            <Nav className="mr-auto nav-collapse nav-collapse-container">        
+                <LinkContainer exact to="/">
+                    <Nav.Link><PizzaIcon className="d-block d-lg-none mr-3"/>VÅRA PIZZOR</Nav.Link>
+                </LinkContainer>
+
+                <LinkContainer to="/menu">
+                    <Nav.Link><MapIcon className="d-block d-lg-none mr-3"/> VÅR meny</Nav.Link>
+                </LinkContainer>
+                
+                <LinkContainer to="/rest">
+                    <Nav.Link><MapMarkerIcon className="d-block d-lg-none mr-3"/> VÅRA RESTAURANGER</Nav.Link>
+                </LinkContainer>
+                
+                <LinkContainer to="/job">
+                    <Nav.Link><ChefHatIcon className="d-block d-lg-none mr-3"/> jobba hos oss</Nav.Link>
+                </LinkContainer>
+                
+                <LinkContainer to="/contact">
+                    <Nav.Link><EmailIcon className="d-block d-lg-none mr-3"/> kontakt</Nav.Link>
+                </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+
+          <Navbar.Brand
+            as={Link}
+            to="/"
+            className="d-none d-lg-block mr-0 navbar-brand-large-logo-container">
+            <img src={logo} alt="React Bootstrap logo"
+              className="d-inline-block align-top navbar-brand-large-logo"
+            />
+          </Navbar.Brand>
+          <Button variant="danger" className="d-none d-lg-block order-online-btn-large"
+            style={{ position: "absolute", right: "10px" }}>
+            ORDER ONLINE
+          </Button>
+        </Navbar>
+    //   </div>
+    );
 }
 
 export default MainMenu
